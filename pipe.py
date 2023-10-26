64d3a2d8da4b53d8103ba040ef2dd1625612cc0d
mandrindra12 <mandrindraantonnio@gmail.com>
2023-10-26 20:16:22 +0300

    removed the display() method to be more generic

--- pipe.py
+++ pipe.py
@@ -1,13 +1,8 @@
-import py5
-
 class Pipe:
-    def __init__(self, x_pos, img, h):
+    def __init__(self, x_pos, h):
         self.x = x_pos
-        self.icon = py5.load_image(img)
         self.h = h
 
     def move(self, vel_x):
         self.x = self.x + vel_x
 
-    def display(self):
-        py5.image(self.icon, self.x, py5.height-self.h, 100, self.h)
