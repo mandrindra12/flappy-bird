d59eaba7b8e81cba407ed86bbacf1edf1051c28f
mandrindra12 <mandrindraantonnio@gmail.com>
2023-10-26 20:25:10 +0300

    removed unused variable

--- pipe.py
+++ pipe.py
@@ -1,7 +1,6 @@
 class Pipe:
-    def __init__(self, x_pos, h):
+    def __init__(self, x_pos):
         self.x = x_pos
-        self.h = h
 
     def move(self, vel_x):
         self.x = self.x + vel_x
