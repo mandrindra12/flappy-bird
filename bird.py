d6a26885cdb9a121308490c92a2dcca5be1291e6
mandrindra12 <mandrindraantonnio@gmail.com>
2023-10-26 20:16:49 +0300

    removed the display() method to be more generic

--- bird.py
+++ bird.py
@@ -1,14 +1,10 @@
-import py5
 
 class Bird:
-    def __init__(self, pos_x, pos_y, img):
+    def __init__(self, pos_x, pos_y):
         self.x = pos_x
         self.y = pos_y
-        self.avatar = py5.load_image(img)
 
     def move(self, vel_x, vel_y):
         self.x = self.x + vel_x
         self.y = self.y + vel_y
 
-    def display(self):
-        py5.image(self.avatar, self.x, self.y, 50, 50)
