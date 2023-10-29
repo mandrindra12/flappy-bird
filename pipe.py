db7c97c154ebd21bb15f484ec076901e37ea8821
mandrindra12 <mandrindraantonnio@gmail.com>
2023-10-29 17:22:09 +0300

    more attribute added for easier manipulation

--- pipe.py
+++ pipe.py
@@ -1,6 +1,11 @@
 class Pipe:
-    def __init__(self, x_pos):
+    def __init__(self, x_pos, y_pos, g):
         self.x = x_pos
+        self.y_upper = 0
+        self.gap = g
+        self.y_lower = y_pos
+        self.lower_height = 0
+        self.upper_height = y_pos-g
 
     def move(self, vel_x):
         self.x = self.x + vel_x
